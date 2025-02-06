4bf150bd1145d3e70ad60c2b4d58cb7e401b594b
Chris Harte <raging.development@gmail.com>
2025-02-06 12:16:53 +0000

    Update LatLong.kt
    No weather reports for the north pole

--- new_weather/LatLong.kt
+++ new_weather/LatLong.kt
@@ -1,23 +1,10 @@
 package com.app.new_weather
 
-import com.app.new_weather.data.Astronomy
-import com.app.new_weather.data.Condition
-import org.json.JSONArray
-
-data class LatLong (var latitude: Double,
-                    var longitude: Double,
-                    var place: String,
-                    var astro: Astronomy,
-                    var cond: Condition,
-                    var temp: String,
-                    var chill: String,
-                    var cond_text: String,
-                    var icon: String,
-                    var forecast_day: JSONArray,
-                    var wind_mph: String,
-                    var wind_dir: String) {
+data class LatLong (var place: String)
+{
     companion object {
         //The location settings were not working on the emulator, so do this.
+        //Also, api_location cannot be empty on the first run through. No weather for the north pole 0,0
         //Ho Chi Minh city just because it is unusual
 //        double latitude = 10.777416;
 //        double longitude = 106.639366;
@@ -30,17 +17,9 @@ data class LatLong (var latitude: Double,
         //Local
 //        double latitude = 53.5333;
 //        double longitude = -2.2833;
-        var latitude: Double = 10.777416
-        var longitude: Double = 106.639366
         var place: String? = ""
-        var astro: Astronomy? = Astronomy()
-        var cond: Condition? = Condition()
-        var temp: String = ""
-        var chill: String = ""
-        var cond_text: String = ""
-        var icon: String = ""
-        var forecast_day = JSONArray()
-        var wind_mph = ""
-        var wind_dir = ""
+        var latitude: Double = 40.7127
+        var longitude: Double = -74.0059
+        var api_location = "${latitude},${longitude}"
     }
 }
