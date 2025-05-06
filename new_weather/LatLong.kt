be5cae05e138b189cfff9baa6970e36a6a68b8d6
Chris Harte <raging.development@gmail.com>
2025-05-06 15:50:42 +0100

    Update LatLong.kt

--- new_weather/LatLong.kt
+++ new_weather/LatLong.kt
@@ -3,13 +3,10 @@ package com.app.new_weather
 data class LatLong (var place: String)
 {
     companion object {
-        //The location settings were not working on the emulator, so do this.
-        //Also, api_location cannot be empty on the first run through. No weather for the north pole 0,0
-
         var place: String? = ""
         var post_code: String? = ""
-        var latitude: Double = 40.7127
-        var longitude: Double = -74.0059
+        var latitude: Double = 0.0
+        var longitude: Double = 0.0
         var api_location = "${latitude},${longitude}"
     }
 }
