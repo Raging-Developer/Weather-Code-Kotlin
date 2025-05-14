b45ed4303b6b28e51791007834f066952f28cf37
Chris Harte <raging.development@gmail.com>
2025-05-14 12:30:46 +0100

    Update FeedLocation.kt
    Refactoring

--- new_weather/data/FeedLocation.kt
+++ new_weather/data/FeedLocation.kt
@@ -2,33 +2,17 @@ package com.app.new_weather.data
 
 import org.json.JSONObject
 
+//Never used, I get my location from the fused location provider.
 class FeedLocation {
-    private var name: String? = null
-    private var region: String? = null
-    private var country: String? = null
-    private var lat: String? = null
-    private var lon: String? = null
-    private var localtime: String? = null
-
-    fun getName(): String? {
-        return name
-    }
-    fun getRegion(): String? {
-        return region
-    }
-    fun getCountry(): String? {
-        return country
-    }
-    fun getLat(): String? {
-        return lat
-    }
-    fun getLon(): String? {
-        return lon
+    companion object{
+        var name: String? = null
+        var region: String? = null
+        var country: String? = null
+        var lat: String? = null
+        var lon: String? = null
+        var localtime: String? = null
     }
 
-    fun getLocaltime(): String? {
-        return localtime
-    }
     @Throws(Exception::class)
     fun populate(data: JSONObject) {
         name = data.optString("name")
