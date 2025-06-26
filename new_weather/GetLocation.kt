d267d90bb9eae1e26121eb2326792345faa9aae3
Chris Harte <raging.development@gmail.com>
2025-06-26 17:46:33 +0100

    Updated GetLocation.kt
    The callback started working so I could lose the rubbish.

--- new_weather/GetLocation.kt
+++ new_weather/GetLocation.kt
@@ -3,10 +3,11 @@ package com.app.new_weather
 import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Geocoder
-import android.location.Location
+import android.os.Looper
 import com.app.new_weather.GetLocation.Companion.callback
 import com.app.new_weather.GetLocation.Companion.fusedClient
 import com.app.new_weather.GetLocation.Companion.locRequest
+import com.app.new_weather.LatLong.Companion.api_location
 import com.app.new_weather.LatLong.Companion.latitude
 import com.app.new_weather.LatLong.Companion.longitude
 import com.app.new_weather.LatLong.Companion.place
@@ -17,14 +18,9 @@ import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
-import com.google.android.gms.tasks.CancellationToken
-import com.google.android.gms.tasks.CancellationTokenSource
-import com.google.android.gms.tasks.OnTokenCanceledListener
 import java.util.Locale
 
-// Just let me use the inbuilt GPS tracker, Pichai does not need to know where I am.
-class GetLocation {
-
+data class GetLocation(var callback: LocationCallback) {
     companion object {
         lateinit var fusedClient: FusedLocationProviderClient
         lateinit var callback: LocationCallback
@@ -34,12 +30,18 @@ class GetLocation {
 
 @SuppressLint("MissingPermission")
 fun get_location(context: Context) {
+    fusedClient = LocationServices.getFusedLocationProviderClient(context)
 
     callback = object : LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult) {
             super.onLocationResult(locationResult)
             for (l in locationResult.locations) {
-                if (l != null) return
+                if (l != null) {
+                    latitude = l.latitude
+                    longitude = l.longitude
+                    api_location = "${latitude},${longitude}"
+                    startLocationUpates(context)
+                }
             }
         }
     }
@@ -48,51 +50,24 @@ fun get_location(context: Context) {
         .setWaitForAccurateLocation(true)
         .build()
 
-    fusedClient = LocationServices.getFusedLocationProviderClient(context) //this is where the location is corrected for the first time.
-    fusedClient.lastLocation
-        .addOnSuccessListener { l: Location? ->
-            if (l != null) {
-                latitude = l.latitude
-                longitude = l.longitude
-            }
-        }
-
-
-    fusedClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY , object : CancellationToken() {
-        override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
-        override fun isCancellationRequested() = false
-    })
-        .addOnSuccessListener { l: Location? ->
-            if (l != null) {
-                latitude = l.latitude
-                longitude = l.longitude
-            }
-        }
-}
-
-@SuppressLint("MissingPermission")
-fun startLocationUpates(context: Context) {
     fusedClient.requestLocationUpdates(locRequest, callback, Looper.getMainLooper())
-    fusedClient.lastLocation
-        .addOnSuccessListener { l: Location? ->
-            if (l != null) {
-                latitude = l.latitude
-                longitude = l.longitude
-                api_location = "${latitude},${longitude}"
-
-                use_lat_and_long(context)
-                getJsonFromAPI(api_location, context as MainActivity)
-            }
-        }
 }
 
-fun use_lat_and_long(context: Context) {
+fun startLocationUpates(context: Context) {
+    use_lat_and_long(context)
+    getJsonFromAPI(api_location, context as MainActivity)
+}
+
+fun use_lat_and_long(context: Context ) {
     var city: String?
     var town: String?
-    var street: String?   
+    var street: String?
+    var number: String?
     val geo = Geocoder(context, Locale.getDefault())
 
-    geo.getFromLocation(latitude, longitude, 1, Geocoder.GeocodeListener
+    //Well no shit goggle, you have deprecated it with no documentation, again!
+    //This is meant to be deprecated and look like this with a listener all as parameters.
+    geo.getFromLocation(latitude, longitude, 1)
     { address_info ->
         city = address_info[0].locality
         town = address_info[0].subLocality
@@ -113,6 +88,10 @@ fun use_lat_and_long(context: Context) {
                 }
             }
         }
-    })
+    }
+}
+
+fun stopLocationUpdates(){
+    fusedClient.removeLocationUpdates(callback)
 }
 
