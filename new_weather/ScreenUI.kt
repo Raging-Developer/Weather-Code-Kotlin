3daccf1100fc28a5b13925b10c38248dec1ab323
Chris Harte <raging.development@gmail.com>
2025-09-05 12:16:39 +0100

    Update ScreenUI.kt
    Now using coroutines and stateflow

--- new_weather/ScreenUI.kt
+++ new_weather/ScreenUI.kt
@@ -1,5 +1,6 @@
 package com.app.new_weather
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.Image
@@ -14,160 +15,147 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.app.new_weather.LatLong.Companion.place
-import com.app.new_weather.LatLong.Companion.post_code
-import com.app.new_weather.data.Astronomy.Companion.moonphase
-import com.app.new_weather.data.Astronomy.Companion.moonrise
-import com.app.new_weather.data.Astronomy.Companion.moonset
-import com.app.new_weather.data.Astronomy.Companion.sunrise
-import com.app.new_weather.data.Astronomy.Companion.sunset
-import com.app.new_weather.data.Condition.Companion.condIcon
-import com.app.new_weather.data.Condition.Companion.condText
-import com.app.new_weather.data.Current.Companion.feelslike_c
-import com.app.new_weather.data.Current.Companion.pressure_mb
-import com.app.new_weather.data.Current.Companion.temp_c
-import com.app.new_weather.data.Current.Companion.wind_dir
-import com.app.new_weather.data.Current.Companion.wind_mph
-import com.app.new_weather.data.Forecast.Companion.forecast_day
 import com.app.new_weather.ui.theme.New_WeatherTheme
-import java.text.SimpleDateFormat
+import kotlinx.coroutines.launch
 
 @Composable
-fun setComposableContent(composeView: ComposeView){
-      composeView.apply {
-        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-          BackGroundImage()
-    }
+//fun setComposableContent(composeView: ComposeView) {
+//    composeView.apply {
+//        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+//        BackGroundImage()
+//    }
+//}
+fun setComposableContent(uiState: WeatherUiState) {
+        BackGroundImage(uiState)
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BackGroundImage() {
+fun BackGroundImage(uiState: WeatherUiState) {
     val backImage = painterResource(R.drawable.night_time)
     val context = LocalContext.current
     val displayMetrics = context.resources.displayMetrics
     val height = displayMetrics.heightPixels
-    var scrollState = rememberScrollState()
-    var isRefreshing by remember{ mutableStateOf(false) }
+    val scrollState = rememberScrollState()
     val state = rememberPullToRefreshState()
+
+    var isRefreshing by remember{ mutableStateOf(false) }
     val coScope = rememberCoroutineScope()
     val onRefresh: () -> Unit = {
         isRefreshing = true
         coScope.launch {
-            delay(1000)
-            isRefreshing = false
+            try {
+                get_location(context as Activity)
+            } finally {
+                isRefreshing = false
+            }
         }
     }
 
-    Image(
-        painter = backImage,
+    Image(painter = backImage,
         contentDescription = null,
         contentScale = ContentScale.FillBounds,
         modifier = Modifier.fillMaxSize()
     )
-    
+
     PullToRefreshBox(
         isRefreshing = isRefreshing,
         onRefresh = onRefresh,
         state = state)
     {
-        Box(Modifier
-        .verticalScroll(scrollState)
-        .height(height.dp),
+        //Scrollable content has to go inside the pullToRefreshBox
+        //But I also need it to scroll differently in landscape. Hmm, think on it.
+        Box(Modifier.verticalScroll(scrollState).height(height.dp),
         contentAlignment = Alignment.TopCenter)
         {
-           if (isRefreshing){
-                 useLocationUpates(context as MainActivity)
-            }
-            WeatherLayout()
+            WeatherLayout(uiState)
         }
     }
 }
 
 @Composable
-fun WeatherLayout( modifier: Modifier = Modifier) {
-    var lcondIcon = condIcon
-    var condString: String
-
-    if (condIcon == null) {
-        lcondIcon = "na"
-    }
-    else {
-        condString = lcondIcon!!.substring(lcondIcon.length - 7)
-        lcondIcon = condString.substring(0, 3)
-    }
-    //This is ugly, but it avoids getIdentifier
+fun WeatherLayout(uiState: WeatherUiState, modifier: Modifier = Modifier) {
+    var lcondIcon = uiState.condIcon
     val iconImage = painterResource(R.drawable::class.java.getField("icon_$lcondIcon").getInt(null))
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.padding(top = 60.dp)) //Make room for the toolbar
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxWidth()
+            .padding(top = 60.dp)) //Make room for the toolbar
     {
-        Image(
-            painter = iconImage,
+        Image(painter = iconImage,
             contentDescription = null,
             modifier = Modifier.size(150.dp))
 //            contentScale = ContentScale.Fit)
 
-        //The only thing I want scrolling is fore_cast
-        Temp_today()
-        Condition_text()
-        Location_text()
-        Fore_cast()
-        SunRiseSet()
-        MoonPhase()
+        //The only thing I want scrolling is fore_cast, except in landscape where it all scrolls
+        Temp_today(uiState.temp_c,
+            uiState.feelsLike,
+            uiState.wind_dir,
+            uiState.wind_mph)
+        Condition_text(uiState.condText, uiState.pressure)
+        Location_text(uiState.vm_place)
+        Fore_cast(uiState.fore_array)
+        SunRiseSet(uiState.sunrise, uiState.sunset)
+        MoonPhase(uiState.moonphase, uiState.moonrise, uiState.moonset)
 
-        Column(
-            modifier = Modifier
+        Column(modifier = Modifier
                 .fillMaxWidth()
-                .weight(1f)
+//                .weight(1f) //I want it at the bottom of the screen not bottom of the layout.
                 .align(Alignment.End)
         ) {}
-        ApiLinkButton()
+        ApiLinkButton(uiState.post_code)
     }
 }
 
 @Composable
-fun Temp_today( modifier: Modifier = Modifier) {
-
-    Box {
-        Row(
-            modifier = Modifier)
-        {
-            Text(
-                text = "$temp_c\u00B0",
-                fontSize = 20.sp,
-                lineHeight = 20.sp,
-                textAlign = TextAlign.Center,
-                modifier = modifier.padding(0.dp),
-                color = Color.LightGray)
-            Chill_factor()
-        }
+fun Temp_today( temp_c: String,
+                feelslike_c: String,
+                wind_dir: String,
+                wind_mph: String,
+                modifier: Modifier = Modifier) {
+    Row(modifier = Modifier)
+    {
+        Text(
+            text = "$temp_c\u00B0",
+            fontSize = 20.sp,
+            lineHeight = 20.sp,
+            textAlign = TextAlign.Center,
+            modifier = modifier.padding(0.dp),
+            color = Color.LightGray
+        )
+        Chill_factor(feelslike_c, wind_dir, wind_mph)
     }
 }
 
 @Composable
-fun Chill_factor(modifier: Modifier = Modifier) {
-    val chill =
-        "(Which feels like $feelslike_c\u00B0 \nin a $wind_dir $wind_mph mph wind)"
-
+fun Chill_factor(feelslike_c: String,
+                 wind_dir: String,
+                 wind_mph: String,
+                 modifier: Modifier = Modifier) {
     Text(
-        text = chill,
+        text = "(Which feels like $feelslike_c\u00B0 \nin a $wind_dir $wind_mph mph wind)",
         fontSize = 12.sp,
         lineHeight = 12.sp,
         textAlign = TextAlign.Center,
@@ -176,8 +164,9 @@ fun Chill_factor(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Condition_text(modifier: Modifier = Modifier) {
-
+fun Condition_text(condText: String,
+                   pressure_mb: String,
+                   modifier: Modifier = Modifier) {
     Text(
         textAlign = TextAlign.Center,
         text = "The weather today is $condText at $pressure_mb mb",
@@ -188,8 +177,8 @@ fun Condition_text(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Location_text(modifier: Modifier = Modifier) {
-
+fun Location_text(place: String,
+                  modifier: Modifier = Modifier) {
     Text(
         text = "Which is not bad for $place",
         fontSize = 20.sp,
@@ -199,30 +188,18 @@ fun Location_text(modifier: Modifier = Modifier) {
         color = Color.LightGray)
 }
 
+//Forecast has the next(?) three days in it.
 @Composable
-fun Fore_cast() {
-    var tomoz_day = ""
-    var tomoz_text = ""
-    var fore_array = mutableListOf<String>()
-
-    for (i in 0 until forecast_day.length()) {
-        val o = forecast_day.getJSONObject(i)
-        val oday = SimpleDateFormat("EEEE, d MMM").format(o.getString("date_epoch").toLong() * 1000)
-        val otext = o.getJSONObject("day").getJSONObject("condition").getString("text")
-
-        if (i == 1) {
-            tomoz_text = otext
-            tomoz_day = oday
-        }
-        fore_array.add("$oday $otext")
-    }
+fun Fore_cast(fore_array: List<Fore_array_items>) {
+    val tomoz_day = fore_array[1].date
+    val tomoz_text = fore_array[1].conditionText
 
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Tomorrow(tomoz_text, tomoz_day)
         LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
-            items(fore_array.count()){
-                index ->
-                Text(text = fore_array[index],
+            items(fore_array.size){ index ->
+                val item = fore_array[index]
+                Text(text = "${item.date} ${item.conditionText}",
                     color = Color.LightGray,
                     fontSize = 12.sp,
                     lineHeight = 12.sp,
@@ -250,8 +227,9 @@ fun Tomorrow(
 }
 
 @Composable
-fun SunRiseSet( modifier: Modifier = Modifier) {
-
+fun SunRiseSet(sunrise: String,
+               sunset: String,
+               modifier: Modifier = Modifier) {
     Column(horizontalAlignment = Alignment.CenterHorizontally)
     {
         Text(
@@ -264,24 +242,33 @@ fun SunRiseSet( modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun MoonPhase( modifier: Modifier = Modifier){
-
-    var moonIcon = when (moonphase) {
-        "First Quarter" -> "firstquarter"
-        "Full Moon" -> "fullmoon"
-        "Last Quarter" -> "lastquarter"
-        "New Moon" -> "newmoon"
-        "Waning Crescent" -> "waningcrescent"
-        "Waning Gibbous" -> "waninggibbous"
-        "Waxing Crescent" -> "waxingcrescent"
-        "Waxing Gibbous" -> "waxinggibbous"
-        else -> {
-            "fullmoon"
+fun MoonPhase( moonphase: String,
+               moonrise: String,
+               moonset: String,
+               modifier: Modifier = Modifier){
+    val moonIcon = remember(moonphase){
+        when (moonphase) {
+            "First Quarter" -> "firstquarter"
+            "Full Moon" -> "fullmoon"
+            "Last Quarter" -> "lastquarter"
+            "New Moon" -> "newmoon"
+            "Waning Crescent" -> "waningcrescent"
+            "Waning Gibbous" -> "waninggibbous"
+            "Waxing Crescent" -> "waxingcrescent"
+            "Waxing Gibbous" -> "waxinggibbous"
+            else -> {
+                "waxingcrescent"
+            }
         }
     }
 
-    Column(horizontalAlignment = Alignment.CenterHorizontally)
-    {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        var moon_text = "With the moon rising at $moonrise and setting at $moonset"
+
+        if (moonrise == "No moonrise"){
+            moon_text = "The moon is up and will set at $moonset"
+        }
+
         Text(
             text = "The phase of the moon is $moonphase",
             fontSize = 12.sp,
@@ -294,8 +281,7 @@ fun MoonPhase( modifier: Modifier = Modifier){
             modifier = Modifier.size(125.dp)
         )
         Text(
-            text = "With the moon rising at $moonrise " +
-                    "and setting at $moonset",
+            text = moon_text,
             fontSize = 12.sp,
             lineHeight = 12.sp,
             color = Color.LightGray)
@@ -303,7 +289,7 @@ fun MoonPhase( modifier: Modifier = Modifier){
 }
 
 @Composable
-fun ApiLinkButton() {
+fun ApiLinkButton(post_code: String) {
     val context = LocalContext.current
     val icon = painterResource(R.drawable.weatherapi_logo)
     val i = Intent()
@@ -313,17 +299,18 @@ fun ApiLinkButton() {
     i.setData(Uri.parse("https://www.weatherapi.com/weather/q/$post_code"))
 
     TextButton(
+        modifier = Modifier.fillMaxWidth(),
         onClick = { context.startActivity(i) },
         content = {
             Image(
                 painter = icon,
                 contentDescription = "Link",
-                modifier = Modifier.size(40.dp)
-                    .weight(1f))
+                modifier = Modifier
+                    .size(40.dp)
+                    .weight(1f)
+            )
         })
 }
-
-
 
 
 
